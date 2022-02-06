e0aeea158df89672867d1984520080dcfe90c40d
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2022-02-06 12:24:28 +0100

    projet ready for pipeline

--- src/main/java/fr/umontpellier/iut/exercice2/PrimeFactors.java
+++ src/main/java/fr/umontpellier/iut/exercice2/PrimeFactors.java
@@ -9,18 +9,7 @@ public class PrimeFactors {
         int dividende=0;
 
 
-       /* if (number % 2 == 0 && number != 2 ) {
-            quotient_n1=quotient;
-            nombre=number;
-            dividende=2;
-            maListeJava.add(dividende);
-            while (quotient_n1!=1){
-                quotient=nombre/dividende;
-                maListeJava.add(quotient);
-                nombre=quotient;
-                quotient_n1=nombre/dividende;
-            }
-        }*/
+
         while (i<=number)
         {
             if (number%i==0) {
@@ -32,76 +21,7 @@ public class PrimeFactors {
             i++;
         }
 
-
-        if (number == 3 || number == 5 || number == 7) {
-            maListeJava.add(number);
-        }
-
-        if (number == 1) { // quand c'est un nombre premier renvoie la liste vide
-
-        }
-
-
-
-/*
-        if (number == 4158) {
-            maListeJava.add(2);
-            maListeJava.add(3);
-            maListeJava.add(3);
-            maListeJava.add(3);
-            maListeJava.add(7);
-            maListeJava.add(11);
-        }*/
-
         return maListeJava;
-       /*
-        if (number == 1) { // quand c'est un nombre premier renvoie la liste vide
 
-        }
-        if (number == 2) {
-            maListeJava.add(2);
-        }
-        if (number == 3) {
-            maListeJava.add(3);
-        }
-        if (number == 4) {
-            maListeJava.add(2);
-            maListeJava.add(2);
-        }
-        if (number == 5) {
-            maListeJava.add(5);
-        }
-        if (number == 6) {
-            maListeJava.add(2);
-            maListeJava.add(3);
-        }
-        if (number == 7) {
-            maListeJava.add(7);
-        }
-        if (number==8){
-            maListeJava.add(2);
-            maListeJava.add(2);
-            maListeJava.add(2);
-
-        }
-        if (number==9){
-            maListeJava.add(3);
-            maListeJava.add(3);
-        }
-        if (number==10){
-            maListeJava.add(2);
-            maListeJava.add(5);
-        }
-        if (number==4158){
-            maListeJava.add(2);
-            maListeJava.add(3);
-            maListeJava.add(3);
-            maListeJava.add(3);
-            maListeJava.add(7);
-            maListeJava.add(11);
-        }
-
-
-        return maListeJava;*/
     }
 }
--- src/test/java/fr/umontpellier/iut/exercice2/PrimeFactorsTest.java
+++ src/test/java/fr/umontpellier/iut/exercice2/PrimeFactorsTest.java
@@ -108,8 +108,18 @@ class PrimeFactorsTest {
     }
 
     @Test
-    public void five_thousand_five_hundred_forty_nine_should_give_2_3_3_3_7_11() {
-        assertEquals(list(2, 3, 3, 3, 7, 11), primeFactors.computeFactors(5549));
+    public void five_thousand_five_hundred_forty_nine_should_give_31_79() {
+        assertEquals(list(31,179), primeFactors.computeFactors(5549));
+    }
+
+    @Test
+    public void sixty_four_thousand_eight_hundred_twenty_one_should_give_3_17_31_41() {
+        assertEquals(list(3, 17,31,41), primeFactors.computeFactors(64821));
+    }
+
+    @Test
+    public void six_hundred_forty_eight_thousand_four_hundred_eighty_six_should_give_2_3_3_3_4003() {
+        assertEquals(list(2,3,3,3,3,4003), primeFactors.computeFactors(648486));
     }
 
 }
