87f423a4c683499ee59d59b1c42abc123d34be67
Mathis Duban <mathis.duban@etu.umonpellier.fr>
2022-02-13 10:32:38 +0100

    ajout classe builder

--- src/main/java/fr/umontpellier/iut/exo3/EtudiantBuilder.java
+++ src/main/java/fr/umontpellier/iut/exo3/EtudiantBuilder.java
@@ -1,4 +1,46 @@
 package fr.umontpellier.iut.exo3;
 
+
+import java.time.LocalDate;
+
+
 public class EtudiantBuilder {
+    private String nom, prenom, adresse_mail, adresse_postale;
+    private LocalDate date_Naissance;
+
+
+    public EtudiantBuilder ajouterNom(String nom){
+        this.nom=nom;
+        return this;
+    }
+
+    public EtudiantBuilder ajouterPrenom(String prennom){
+        this.prenom=prennom;
+        return this;
+    }
+
+    public EtudiantBuilder ajouterAdressePostale(String adresse){
+        this.adresse_postale=adresse;
+        return this;
+    }
+
+    public EtudiantBuilder ajouterMail(String mail){
+        this.adresse_mail=mail;
+        return this;
+    }
+
+    public EtudiantBuilder ajouterDateNaissance(LocalDate date){
+        this.date_Naissance=date;
+        return this;
+    }
+    public EtudiantBuilder build(){
+        EtudiantBuilder etu=new EtudiantBuilder();
+        etu.nom=nom;
+        etu.prenom=prenom;
+        etu.adresse_mail=adresse_mail;
+        etu.adresse_postale=adresse_postale;
+        etu.date_Naissance=date_Naissance;
+        return etu;
+    }
+
 }
